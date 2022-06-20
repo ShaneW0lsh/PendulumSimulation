e925e2442f205ecece927f69fefb4e10343c18ad
Shane Walsh <shanebetterfather@gmail.com>
2022-06-20 17:23:47 +0300

    setup new bar

--- src/sketch.js
+++ src/sketch.js
@@ -1,22 +1,27 @@
+const g = 0.8;
+
 var pendulum, bar;
 var mechanicEnergy;
 var pulse = 0.0;
-const g = 0.8;
 
 function setup() { 
-    createCanvas(1900, 925);
+    createCanvas(1525, 925);
 
+    noStroke();
     stroke(255, 255, 255);
     fill(255, 255, 255);
     textSize(35);
     drawingContext.shadowColor = color(255, 255, 255);
 
     pendulum = new Pendulum(createVector(width/2, height/4), 3, 475, PI/8);
-    bar = new EnergyBar(0, 60, 20, color(243, 179, 117), color(0, 0, 0), 3, 2);
+
+    let kEColor = color(240, 84, 84);
+    let pEColor = color(78, 116, 153);
+    bar = new EnergyBar(0, 0, width, 30, kEColor, pEColor, 0, 0);
 }
 
 function draw() { 
-    background(33, 33, 46);
+    background(28, 33, 41);
 
     drawingContext.shadowBlur = sin(pulse) * 7 + 11;
     pulse += 0.09;
@@ -24,19 +29,15 @@ function draw() {
     pendulum.update();
 
     mechanicEnergy = pendulum.getEnergy();
-    //bar.show();
-
-    rect(120, height-150, 30, -int(mechanicEnergy.x));
-    rect(160, height-150, 30, -int(mechanicEnergy.y));
-    text("K", 120, height-118);
-    text("P", 160, height-118);
+    bar.update(mechanicEnergy.x, mechanicEnergy.y);
+    bar.show();
 
     push();
-    fill(243, 179, 117); 
-    stroke(243, 179, 117);
+    fill(221, 221, 221); 
+    stroke(221, 221, 221);
     strokeWeight(10);
     drawingContext.shadowBlur = sin(pulse) * 12 + 17;
-    drawingContext.shadowColor = color(243, 179, 117);
+    drawingContext.shadowColor = color(221, 221, 221);
     pendulum.show();
     pop();
 }
